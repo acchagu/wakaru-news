6a3b0e884413d9450a0ec5fe879c16eab593c710
acchagu <kewozkewoz2@gmail.com>
2026-09-15 16:02:37 +0900

    Update fetch_news.py

--- fetch_news.py
+++ fetch_news.py
@@ -12,6 +12,7 @@ FEEDS = [
     ("お金", "日本銀行", "https://www.boj.or.jp/rss/whatsnew.xml"),
     ("お金", "日本銀行 統計", "https://www.boj.or.jp/rss/statistics.xml"),
     ("お金", "金融庁", "https://www.fsa.go.jp/fsaNewsListAll_rss2.xml"),
+    ("海外", "国連ジュネーブ", "https://www.ungeneva.org/en/news-media/press-items-list/rss.xml"),
 ]
 
 EXCLUDE_WORDS = [
